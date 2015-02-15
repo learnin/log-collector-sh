2b9c0e9d30ecb6c6abdc55adc95c8c9646b1eabf
learnin <i.learnin@gmail.com>
2015-02-16 00:20:02 +0900

    Implement specify target file extension, remove audit log files

--- collector_oracle_audit_log.sh
+++ collector_oracle_audit_log.sh
@@ -10,9 +10,11 @@ shouldTransferFileCount=0
 uname=`uname`
 prefix=`hostname`_`date '+%Y%m%d%H%M%S'`_
 
-# FIXME 対象ファイル名を絞る
-for targetFileName in `ls $TARGET_LOG_DIR`
-do
+targetFileList=${TMP_DIR}/target_file_list.txt
+ls ${TARGET_LOG_DIR}/*.aud > $targetFileList
+ls ${TARGET_LOG_DIR}/*.xml >> $targetFileList
+
+while read targetFileName; do
   md5file=${TMP_DIR}/${targetFileName}.end
 
   if [ "$uname" = 'SunOS' ]; then
@@ -22,11 +24,11 @@ do
   fi
   mputCmd="${mputCmd} ${targetFileName} ${md5file}"
   shouldTransferFileCount=`expr $shouldTransferFileCount + 2`
-done
+done < $targetFileList
 
 nmapCmd='nmap $0 '${prefix}'$0'
 
-ftpLog=$TMP_DIR/ftp.log
+ftpLog=${TMP_DIR}/ftp.log
 
 ftp -inv <<EOF > $ftpLog
 open $FTP_HOST
@@ -45,6 +47,34 @@ if [ `egrep -c "^4|^6" $ftpLog` -ne 0 ]; then
   echo "ftp Error"
 fi
 
-#TODO 掴まれていないログファイルの削除
+while read targetFileName; do
+  # Oracle監査ログファイル名フォーマット
+  # 11.2.0/11.1.0.7/10.2.0.5以降は
+  # <インスタンス名>_<プロセス名>_<プロセス番号>_<シリアル番号>.<拡張子>
+  # それ以前は
+  # <プロセス名>_<プロセス番号>.<拡張子>
+  length=`echo $targetFileName | awk -F"_" '{print NF}'`
+  if [ $length -eq 4 ]; then
+    oraclePid=`echo $targetFileName | awk -F"_" '{print $3}'`
+  elif [ $length -eq 2 ]; then
+    oraclePid=`echo $targetFileName | awk -F"_" '{print $2}' | awk -F"." '{print $1}'`
+  fi
+  if [ `ps -p $oraclePid | wc -l` -eq 1 ]; then
+    # 1行だけ = ヘッダー行のみ = プロセスは生きていないので削除する。
+    rm -f $targetFileName
+  else
+    # プロセスが存在してもPIDがリサイクルされているだけの可能性があるため、
+    # そのプロセスがこのログを開いているか確認する。
+    if [ `type lsof >/dev/null 2>&1; echo $?` -eq 0 ]; then
+      if [ `lsof -p $oraclePid | grep -c $targetFileName` -eq 0 ]; then
+        rm -f $targetFileName
+      fi
+    elif [ `type pfiles >/dev/null 2>&1; echo $?` -eq 0 ]; then
+      if [ `pfiles $oraclePid | grep -c $targetFileName` -eq 0 ]; then
+        rm -f $targetFileName
+      fi
+    fi
+  fi
+done < $targetFileList
 
 exit 0
