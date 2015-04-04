8a813624831b22987de49f43a2583f3c4c1e310b
learnin <i.learnin@gmail.com>
2015-04-04 22:19:38 +0900

    Add logic to detect newer audit log files than last collected

--- agent/collector_oracle_audit_log.sh
+++ agent/collector_oracle_audit_log.sh
@@ -21,6 +21,28 @@ hasError="false"
 prefix=`hostname`_`date '+%Y%m%d%H%M%S'`_
 targetFileList=${TMP_DIR}/target_file_list.txt
 
+# Solaris の sh の内部 test コマンドは -nt をサポートしていないため、
+# そういった比較を行う場合は、/bin/test を使用する。
+testCmd=test
+if [ `uname` = 'SunOS' ]; then
+  testCmd=/bin/test
+fi
+
+# 監査ログファイルが更新されているかの判別を test -nt で行うため、
+# 前回収集日時管理ファイルの更新日時を管理ファイルの内容の日時で更新
+if [ -f $LAST_COLLECT_DATETIME_FILE ]; then
+  last_collect_datetime=`cat $LAST_COLLECT_DATETIME_FILE`
+else
+  last_collect_datetime="200001010000.00"
+fi
+touch -t $last_collect_datetime $LAST_COLLECT_DATETIME_FILE
+
+# 処理開始
+now=`date '+%Y%m%d%H%M.%S'`
+# $now の時刻に秒単位で同時に出力される監査ログエントリが複数レコードあった場合に
+# 一部だけが転送されてしまい、かつ次回の処理対象からも漏れてしまうことを防止するため1秒待つ
+sleep 1
+
 ls ${TARGET_LOG_DIR}/*.aud > $targetFileList 2> /dev/null
 ls ${TARGET_LOG_DIR}/*.xml >> $targetFileList 2> /dev/null
 
@@ -37,24 +59,27 @@ do
     logFileStatus="inactive"
   fi
 
-  # ログファイルを転送
-  targetFileName=`basename $targetFilePath`
-  doFtpPut $targetFilePath ${FTP_DIR}/${prefix}${targetFileName} ${TMP_DIR}/ftp.log $FTP_SUCCESS_MSG
-  if [ $? -ne 0 ]; then
-    echo "ftp Error. file=${targetFilePath}"
-    hasError="true"
-    continue
-  fi
+  # 監査ログファイルの更新日時 > 前回収集日時 の場合は転送
+  if $testCmd $targetFilePath -nt $LAST_COLLECT_DATETIME_FILE; then
+    # ログファイルを転送
+    targetFileName=`basename $targetFilePath`
+    doFtpPut $targetFilePath ${FTP_DIR}/${prefix}${targetFileName} ${TMP_DIR}/ftp.log $FTP_SUCCESS_MSG
+    if [ $? -ne 0 ]; then
+      echo "ftp Error. file=${targetFilePath}"
+      hasError="true"
+      continue
+    fi
 
-  # 転送完了通知用ファイルを転送
-  endFilePath=${TMP_DIR}/${prefix}`echo $targetFileName | awk -F . '{print $1}'`.end
-  touch $endFilePath
-  endFileName=`basename $endFilePath`
-  doFtpPut $endFilePath ${FTP_DIR}/${endFileName} ${TMP_DIR}/ftp.log $FTP_SUCCESS_MSG
-  if [ $? -ne 0 ]; then
-    echo "ftp Error. file=${endFilePath}"
-    hasError="true"
-    continue
+    # 転送完了通知用ファイルを転送
+    endFilePath=${TMP_DIR}/${prefix}`echo $targetFileName | awk -F . '{print $1}'`.end
+    touch $endFilePath
+    endFileName=`basename $endFilePath`
+    doFtpPut $endFilePath ${FTP_DIR}/${endFileName} ${TMP_DIR}/ftp.log $FTP_SUCCESS_MSG
+    if [ $? -ne 0 ]; then
+      echo "ftp Error. file=${endFilePath}"
+      hasError="true"
+      continue
+    fi
   fi
 
   # 既に解放されているログファイルだった場合は削除
@@ -68,4 +93,8 @@ exec 0<&9 9<&-
 if [ $hasError = "true" ]; then
   exit 1
 fi
+
+# 前回収集日時管理ファイルを更新
+echo $now > $LAST_COLLECT_DATETIME_FILE
+
 exit 0
--- agent/config.profile
+++ agent/config.profile
@@ -5,3 +5,4 @@ FTP_PASSWORD=ftppass
 FTP_DIR=/
 FTP_SUCCESS_MSG="226 Transfer complete"
 TMP_DIR=tmp
+LAST_COLLECT_DATETIME_FILE=last_collect_datetime
